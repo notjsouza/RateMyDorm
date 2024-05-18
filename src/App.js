9d8fc619321135a48b973adf770bfa0fdb6ef325
Justin <44349024+notjsouza@users.noreply.github.com>
2024-05-18 01:30:34 -0700

    created react project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button>This is a Button</button>
         <a
           className="App-link"
           href="https://reactjs.org"
